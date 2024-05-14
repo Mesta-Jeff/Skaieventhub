c73e6ea4762e8c8ec39736f2787856318f14ac6c
Mesta-Jeff <odenehonas1@gmail.com>
2024-05-14 09:21:29 +0000

    Third update after second sync

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -93,7 +93,7 @@
                                             <h2 class="text-xl font-semibold text-black dark:text-white">Documentation</h2>
 
                                             <p class="mt-4 text-sm/relaxed">
-                                                Laravel has wonderful documentation covering every aspect of the framework. Whether you are a newcomer or have prior experience with Laravel, we recommend reading our documentation from beginning to end.
+                                                Laravel has wonderful documentation covering every aspect of the framework. Whether you are a newcomer or have prior experience with Laravel, we recommend reading our documentation from beginning to end all complete.
                                             </p>
                                         </div>
                                     </div>
